1a54645a6a2cfdb1d8d53550e65f25552e21ac9a
David Adsit <david.adsit@gmail.com>
2012-08-01 19:02:41 -0600

    Additional fitnesse test

---
+++ Fixtures/FinalScore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace BowlingKata.Fixtures
+{
+    public class FinalScore
+    {
+        private string[] _rolls;
+
+        public void Rolls(string rolls)
+        {
+            _rolls = rolls.Trim().Split(' ');
+        }
+
+        public string Score()
+        {
+            var game = new Game();
+            foreach (int roll in _rolls.Select(x => Convert.ToInt32(x)))
+            {
+                game.Roll(roll);
+            }
+            return game.Score().ToString(CultureInfo.CurrentCulture);
+        }
+    }
+}
